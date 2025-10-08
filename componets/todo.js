9a151b865d58a5207ce958c399730976de8bf1fe
Seth Taylor <taylorseth59@gmail.com>
2025-10-08 15:24:07 -0500

    update

---
+++ componets/todo.js
@@ -0,0 +1,20 @@
+class Todo {
+  constructor(data, selector) {
+    this._data = data;
+    this._templateElement = document.querySelector(selector);
+  }
+
+  getView() {
+    const todoElement = this._templateElement.content
+      .querySelector(".todo")
+      .cloneNode(true);
+    const todoNameEl = todoElement.querySelector(".todo__name");
+    const todoCheckboxEl = todoElement.querySelector(".todo__completed");
+    const todoLabel = todoElement.querySelector(".todo__label");
+    const todoDate = todoElement.querySelector(".todo__date");
+    const todoDeleteBtn = todoElement.querySelector(".todo__delete-btn");
+    console.log("i got called");
+  }
+}
+
+export default Todo;
